18e53daf826d4bcb031cf0cea96f0c28d7feee8e
Pedro <alumno.194614@iesazarquiel.es>
2022-06-17 11:32:23 +0200

    subo clientes-app

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso de Spring 5 con Angular';
+  profesor: string = 'Daniel Guzman';
+
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+   { id: 1, nombre: 'Pedro', apellido: 'Esteban', email: 'pedro@pedro.com', createAt: '17/04/2022' },
+   { id: 2, nombre: 'Pedro2', apellido: 'Esteban2', email: 'pedro2@pedro.com', createAt: '17/04/2022' },
+   { id: 3, nombre: 'Pedro3', apellido: 'Esteban3', email: 'pedro3@pedro.com', createAt: '17/04/2022' },
+   { id: 4, nombre: 'Pedro4', apellido: 'Esteban4', email: 'pedro4@pedro.com', createAt: '17/04/2022' },
+   { id: 5, nombre: 'Pedro5', apellido: 'Esteban5', email: 'pedro5@pedro.com', createAt: '17/04/2022' },
+   { id: 6, nombre: 'Pedro6', apellido: 'Esteban6', email: 'pedro6@pedro.com', createAt: '17/04/2022' },
+   { id: 7, nombre: 'Pedro7', apellido: 'Esteban7', email: 'pedro7@pedro.com', createAt: '17/04/2022' },
+   { id: 8, nombre: 'Pedro8', apellido: 'Esteban8', email: 'pedro8@pedro.com', createAt: '17/04/2022' },
+   { id: 9, nombre: 'Pedro9', apellido: 'Esteban9', email: 'pedro9@pedro.com', createAt: '17/04/2022' },
+   { id: 10, nombre: 'Pedro10', apellido: 'Esteban10', email: 'pedro10@pedro.com', createAt: '17/04/2022' },
+];
